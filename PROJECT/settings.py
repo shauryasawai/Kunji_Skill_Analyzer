e4592f9393d5678d9b758c5966501c4ca3d266af
Shauryaman Sawai <sawaisushil@gmail.com>
2026-04-27 13:22:06 +0530

    fixed_security

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -52,9 +52,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'base.middleware.SecurityHeadersMiddleware',
-]
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',]
 
 ROOT_URLCONF = 'PROJECT.urls'
 
@@ -79,23 +77,23 @@ WSGI_APPLICATION = 'PROJECT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# import dj_database_url
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
 # DATABASES = {
-#     'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=False)
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+import dj_database_url
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=False)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -215,6 +213,8 @@ FILE_UPLOAD_DIRECTORY_PERMISSIONS = 0o755
 CSRF_FAILURE_VIEW = 'django.views.csrf.csrf_failure'
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_NAME = 'csrftoken'
+
+
 
 from dotenv import load_dotenv
 import os
